ee8cc002973863dbcff43b7fd4a247007ac07597
Clara Cheong <clarawhcheong@gmail.com>
2019-12-14 20:28:32 -0500

    home page + git ignore node modules

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,24 @@
 // Home.js
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import { Grid, Typography } from '@material-ui/core';
+
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <h2>Home Page</h2>
+        <br></br>
+        <Grid container direction='row' justify='center' alignItems='center'>
+          <Grid item>
+            <Typography variant='h2'>Home</Typography>
+          </Grid>
+        </Grid>
+        <Grid container direction='row' justify='center' alignItems='baseline'>
+          <p>Description about project goes here.</p>
+        </Grid>
+
       </div>
     );
   }
