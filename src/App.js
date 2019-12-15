106bd9ee31eb7335cfa0332dfd4c469bb2318851
mshan10 <mshan@nd.edu>
2019-12-15 14:58:18 -0500

    added new styling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
 // App.js
 
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import Bookmarks from '@material-ui/icons/Bookmarks';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import {
   Switch,
   Route,
   BrowserRouter,
 } from "react-router-dom";
 
-import Navbar from './Navbar';
 import Home from './Home';
 import MachineLearning from './MachineLearning';
 import Analytics from './Analytics';
+
+// Find default themes here 
+// https://material-ui.com/customization/default-theme/
+
+// Custom theme override
+const theme = createMuiTheme({
+  palette: {
+      type: 'dark',
+      primary: {
+          light: '#63ccff',
+          main: '#ad7b05',
+          dark: '#eb4034',
+          contrastText: '#fff',
+      },
+      text: {
+          primary: "rgba(255, 255, 255, 0.87)",
+          secondary: "rgba(255, 255, 255, 0.54)",        
+          disabled: "rgba(255, 255, 255, 0.38)",
+          hint: "rgba(255, 255, 255, 0.38)",
+      },
+      background: {
+          paper: "#fff",
+          default: "#000"
+      }
+  },
+});
 
 class App extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/MachineLearning" component={MachineLearning} />
-            <Route path="/Analytics" component={Analytics} />
-            <Route path="/" component={Home} />
-          </Switch>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Switch>
+              <Route path="/MachineLearning" component={MachineLearning} />
+              <Route path="/Analytics" component={Analytics} />
+              <Route path="/" component={Home} />
+            </Switch>
+          </BrowserRouter>
+        </ThemeProvider>
       </div>
     );
   }
--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,47 @@
 // Home.js
 import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles'
 import Navbar from './Navbar'
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Button } from '@material-ui/core';
+import aikoo from './images/aikoo.gif'
+import { withTheme } from '@material-ui/core/styles';
 
+export default withStyles((theme) => ({
+  
+  card: {
+    minWidth: 275,
+  },
+  gif: {
+    width: 300,
+    height: 250
+  },
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    color: theme.palette.text.primary,
+    height: '100vh'
+  }
+}))(class extends Component {
+  
+  state = {
+    teams: [],
+  }
 
-class Home extends Component {
   render() {
+    const { classes } = this.props
     return (
-      <div>
+      <div className={classes.root}>
         <Navbar />
         <br></br>
-        <Grid container direction='row' justify='center' alignItems='center'>
-          <Grid item>
-            <Typography variant='h2'>Home</Typography>
-          </Grid>
+        <Grid container direction='row' justify='center' alignItems='center' spacing={24}>
+          <img src={aikoo} className={classes.gif} />
+          <Typography variant='h2'>Home</Typography>
         </Grid>
         <Grid container direction='row' justify='center' alignItems='baseline'>
           <p>Description about project goes here.</p>
         </Grid>
-
       </div>
-    );
+    )
   }
-}
 
-export default Home;
+})
